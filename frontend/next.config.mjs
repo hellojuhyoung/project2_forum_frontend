4e831fb10714394e88576e83237e117214e03860
hellojuhyoung <juhyoung.leee@gmail.com>
2025-07-14 00:56:28 +0900

    fix: redirect error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,16 +4,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        // Use an environment variable for the backend API URL
-        // Make sure to set NEXT_PUBLIC_BACKEND_URL in your Amplify Console!
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       // Use an environment variable for the backend API URL
+  //       // Make sure to set NEXT_PUBLIC_BACKEND_URL in your Amplify Console!
+  //       destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+  //     },
+  //   ];
+  // },
 
   // Environment variables that should be available on the client-side
   // Note: NEXT_PUBLIC_API_URL is already defined in your env,
